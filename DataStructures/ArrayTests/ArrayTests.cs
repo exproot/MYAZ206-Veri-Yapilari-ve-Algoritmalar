5198bdff2c27cd7bc1aac812b29d2e5233da6040
Ertan Yagmur <ertanyagmur@outlook.com>
2023-03-21 15:09:51 +0300

    week02 added.

--- DataStructures/ArrayTests/ArrayTests.cs
+++ DataStructures/ArrayTests/ArrayTests.cs
@@ -220,5 +220,56 @@ namespace ArrayTests
 
             Assert.Equal(4, array.Capacity);
         }
+
+        [Fact]
+        public void Array_GetEnumerator_Test()
+        {
+            // Arrange
+            var array = new Array.Array();
+            array.Add("Ahmet");
+            array.Add("Mehmet");
+            array.Add("Can");
+
+            string result = "";
+            foreach (var item in array)
+            {
+                result = string.Concat(result, item);
+            }
+
+            Assert.Equal("AhmetMehmetCan", result);
+
+        }
+        [Fact]
+        public void Array_Constructor_Test()
+        {
+            // Arrange
+            var array = new Array.Array(36,23,55,44,61);
+
+            // Act
+            var result = array.Capacity; // 5
+            
+            var result2 = String.Empty;
+            foreach (var item in array)
+            {
+                result2 = string.Concat(result2, item);
+            }
+
+            // Assert
+            Assert.Equal(5, result);
+            Assert.Equal("3623554461", result2);
+        }
+        [Fact]
+        public void Array_SetItem_Test()
+        {
+            // Arrange
+            var numbers = new Array.Array(1, 3, 5, 7);
+
+            // Act
+            numbers.SetItem(2, 55);
+
+            // Assert
+            Assert.Equal(55,numbers.GetItem(2));
+            Assert.True(numbers.GetItem(2).Equals(55));
+        }
     }
 }
