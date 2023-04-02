e569c2329e79845da501f0f9650279e18848b79a
Ertan Yagmur <ertanyagmur@outlook.com>
2023-04-02 16:44:35 +0300

    Hafta04(lab dahil) eklendi.

---
+++ DataStructures/LinkedList/Singly/SinglyLinkedListNode.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedList.Singly;
+
+public class SinglyLinkedListNode<T>
+{
+    public T? Val {get; set;}
+    public SinglyLinkedListNode<T> Next { get; set; }
+
+    public SinglyLinkedListNode() { }
+    public SinglyLinkedListNode(T value)
+    {
+        Val = value;
+    }
+
+    public override string ToString() {
+        return $"{Val}";
+    }
+}
